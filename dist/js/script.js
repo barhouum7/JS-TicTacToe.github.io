96dbe8894ae0ae2d6846e9d45f8696c92265074d
barhouum7 <bb.med2000@gmail.com>
2020-05-17 12:35:36 +0100

    Start coding JS and added function for handle the clicks

---
+++ dist/js/script.js
@@ -0,0 +1,32 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const X_SHADOW_CLASS = 'x'
+const CIRCLE_SHADOW_CLASS = 'circle'
+const board = document.getElementById('board')
+const cellElements = document.querySelectorAll('[data-cell]')
+
+const userMarkChoice = prompt("Hi Dear Hero, You Want To Play By `X` or `O`", "X or O")
+
+
+let circleMarkTurn
+placeShadowMark()
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(event) {
+    let cell = event.target
+    let currentMark = circleMarkTurn ? CIRCLE_CLASS : X_CLASS
+
+    // 1 - PlaceMark
+    placeMark(cell, currentMark)
+
+    // 2 - Check For Wins?
+    // 3 - Check For Drow?
+    // 4 - Else Switch Turns
+    swapMark()
+    // 0 - Place Shadow of Mark
+    placeShadowMark()
+
+}
