5a19d17705da4271a9c3dac42f96cab9575a224f
barhouum7 <bb.med2000@gmail.com>
2020-05-19 04:10:50 +0100

    Synchronize the user's info with the game

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,6 +4,7 @@ const X_SHADOW_CLASS = 'x'
 const CIRCLE_SHADOW_CLASS = 'circle'
 const board = document.getElementById('board')
 const cellElements = document.querySelectorAll('[data-cell]')
+const userInfo = document.querySelector('.infos')
 
 const userMarkChoice = prompt("Hi Dear Hero, You Want To Play By `X` or `O`", "X or O")
 
@@ -16,6 +17,15 @@ function startGame() {
     if (userMarkChoice.toUpperCase() === 'O') {
         circleMarkTurn = true
     }
+
+    if (userMarkChoice.toUpperCase() === 'X') {
+        userInfo.innerHTML = '<b>Xs</b> For <b>You</b><b><br>vs<br></b><b>Os</b> For The <b>AI</b><-( Soon )'
+    } else if (userMarkChoice.toUpperCase() === 'O') {
+        userInfo.innerHTML = '<b>Os</b> For <b>You</b><b><br>vs<br></b><b>Xs</b> For The <b>AI</b><-( Soon )'
+    } else {
+        userInfo.innerHTML = '<b>Xs</b> For <b>You</b><b><br>vs<br></b><b>Os</b> For The <b>AI</b><-( Soon )'
+    }
+
     cellElements.forEach(cell => {
         cell.addEventListener('click', handleClick, { once: true })
     })
@@ -45,18 +55,10 @@ function placeMark(cell, currentMark) {
 function placeShadowMark() {
     board.classList.remove(CIRCLE_SHADOW_CLASS)
     board.classList.remove(X_SHADOW_CLASS)
-    if (userMarkChoice.toUpperCase() !== 'X') {
-        if (circleMarkTurn) {
-            board.classList.add(CIRCLE_SHADOW_CLASS)
-        } else {
-            board.classList.add(X_SHADOW_CLASS)
-        }
+    if (circleMarkTurn) {
+        board.classList.add(CIRCLE_SHADOW_CLASS)
     } else {
-        if (circleMarkTurn) {
-            board.classList.add(CIRCLE_SHADOW_CLASS)
-        } else {
-            board.classList.add(X_SHADOW_CLASS)
-        }
+        board.classList.add(X_SHADOW_CLASS)
     }
 }
 
