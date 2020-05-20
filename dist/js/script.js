d4bf9e4e6857568abce62a4b462c4e9f0e4b1edd
barhouum7 <bb.med2000@gmail.com>
2020-05-20 01:26:57 +0100

    Adding an Array for all of the winning combinations

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,16 @@ const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 const X_SHADOW_CLASS = 'x'
 const CIRCLE_SHADOW_CLASS = 'circle'
+const WINNING_CONBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const board = document.getElementById('board')
 const cellElements = document.querySelectorAll('[data-cell]')
 const userInfo = document.querySelector('.infos')
